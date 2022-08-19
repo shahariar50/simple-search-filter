b0444eb7e163ae75f23366edec4179c702e63311
shahariar50 <shahariar.touhid@gmail.com>
2022-08-19 13:55:06 +0600

    initiate project and setup dark theme

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Paper } from "@mui/material";
+
+const Header = () => {
+  return (
+    <Paper sx={{ padding: "20px 0" }}>
+      <Container></Container>
+    </Paper>
+  );
+};
+
+export default Header;
